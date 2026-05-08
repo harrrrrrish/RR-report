d52f559e83b3ff2e71bec12019124c68dee42c62
harrrrrrish <157510766+harrrrrrish@users.noreply.github.com>
2026-05-08 16:01:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 # PAGE CONFIG
 # =========================================================
 st.set_page_config(
-    page_title="CT Failure Analysis Dashboard",
+    page_title="RR Analysis APP",
     page_icon="📊",
     layout="wide"
 )
